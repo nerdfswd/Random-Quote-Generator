68e0a6f4320c0904cf4b646ec657c884d27ab8da
Sabiya Tabassum <50179634+sabiyatabassum@users.noreply.github.com>
2021-01-27 23:46:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+var quotes = [
+  ' "The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela ',
+  ' "The way to get started is to quit talking and begin doing." -Walt Disney',
+  " \"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.\" -Steve Jobs",
+  ' "If life were predictable it would cease to be life, and be without flavor." -Eleanor Roosevelt',
+  " \"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.\" -Oprah Winfrey",
+  " \"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.\" -James Cameron",
+  ' "Life is what happens when you\'re busy making other plans." -John Lennon',
+  ' "When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt',
+  ' "Always remember that you are absolutely unique. Just like everyone else." -Margaret Mead',
+  ' "Don\'t judge each day by the harvest you reap but by the seeds that you plant." -Robert Louis Stevenson',
+  ' "The future belongs to those who believe in the beauty of their dreams." -Eleanor Roosevelt',
+  ' "Tell me and I forget. Teach me and I remember. Involve me and I learn." -Benjamin Franklin',
+  ' "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart." -Helen Keller',
+  ' "It is during our darkest moments that we must focus to see the light." -Aristotle',
+  ' "Whoever is happy will make others happy too." -Anne Frank',
+  ' "Do not go where the path may lead, go instead where there is no path and leave a trail." -Ralph Waldo Emerson',
+  ' "You will face many defeats in life, but never let yourself be defeated." -Maya Angelou',
+  ' "Only a life lived for others is a life worthwhile." -Albert Einstein',
+  '  "You only live once, but if you do it right, once is enough." -Mae West',
+  ' "Life is really simple, but we insist on making it complicated." -Confucius',
+  ' "Life is a long lesson in humility." -James M. Barrie',
+  ' "Life is made of ever so many partings welded together." -Charles Dickens',
+  ' "Many of life\'s failures are people who did not realize how close they were to success when they gave up." -Thomas A. Edison'
+];
+var button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  var randomQuote = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote-display").innerHTML = quotes[randomQuote];
+  // var tweetQuote = quotes[randomQuote].split(" ").join("%20");
+  // tweetQuote = tweetQuote.split("<br>").join("");
+  // tweetQuote =
+  //   "https://twitter.com/intent/tweet?text=" + tweetQuote.split('"').join("");
+  // $(".twitter-share-button").attr("href", tweetQuote);
+});
